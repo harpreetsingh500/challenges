6318dfe8dd82e3c63cb51d4a0d070b0b6c15ca4f
Harpreet Singh <harpeetsingh500@gmail.com>
2016-04-28 03:49:37 +0000

    Changed line 3 & 31

--- simple_cipher/simple_cipher.rb
+++ simple_cipher/simple_cipher.rb
@@ -1,12 +1,7 @@
 class Cipher
   attr_reader :key
-  KEYS = { 'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4,
-           'f' => 5, 'g' => 6, 'h' => 7, 'i' => 8, 'j' => 9,
-           'k' => 10, 'l' => 11, 'm' => 12, 'n' => 13,
-           'o' => 14, 'p' => 15, 'q' => 16, 'r' => 17,
-           's' => 18, 't' => 19, 'u' => 20, 'v' => 21,
-           'w' => 22, 'x' => 23, 'y' => 24, 'z' => 25 }
-  
+  KEYS = [*'a'..'z'].map.with_index { |char, idx| [char, idx] }.to_h
+          
   def initialize(key = nil)
     raise ArgumentError if check(key)
     @key = (key == nil) ? set(key) : key
@@ -33,7 +28,7 @@ class Cipher
   def decode(text)
     text.split('').map.with_index do |char, index|
       value = KEYS[char] - KEYS[key[index]]
-      (value < 0) ? KEYS.to_a[value].first : KEYS.key((value).abs)
+      (value < 0) ? KEYS.to_a[value].first : KEYS.key(value)
     end.join
   end
 end
